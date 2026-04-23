1b445920bb55b5a99784beabd6f58b1d96f7fcd0
Manmohit Bisht <manmohit.454@gmail.com>
2026-04-23 23:01:02 +0530

    made cli terminal continuously running and successfully changing directories inside process

--- myparser/main.py
+++ myparser/main.py
@@ -1,27 +1,57 @@
 import argparse
 from pathlib import Path
+from shlex import split
+import os
+
+def cd(path):
+    os.chdir(path)
+
+commands = {
+    'cd' : cd,
+}
+
+def manipulation():
+    while(True):
+        ip = input('')
+        if ip=='quit' or ip=='exit':
+            break
+        
+        tokens = split(ip)
+        if len(tokens) == 3:
+            command = tokens[0]
+            flag = tokens[1]
+            arguments = tokens[2]
+        elif len(tokens) == 2:
+            command = tokens[0]
+            arguments = tokens[1]
+        else:
+            command = tokens[0]
+        
+        if command in commands:
+            commands['cd'](arguments)
+        else:
+            print("Not a valid command")
+        
+        print(Path.cwd(),'$>',end=' ')
+        
 
 def main():
     parser = argparse.ArgumentParser(
                     prog='main.py',
-                    description='Prints the parent directory',
+                    description='Start the CLI file manager',
                     epilog='End of the help!!')
 
 
     parser.add_argument(
-        "-pd", "--file", metavar="folder_or_file_name", help="name of the folder or file"
-    )
-
-    parser.add_argument(
-        '-cwd', "--cwd", action="store_true" ,help="Prints the current working directory"
+        '-start_myp', '--myp', action='store_true', help='name of the folder or file'
     )
     args = parser.parse_args()
+    
+    os.chdir(r'C:\Users\manmo')
+    
+    if args.myp:
+        print(Path.cwd(),'$>',end=' ')
+        manipulation()
 
-    if args.file:
-        parent_dir = Path(args.file).resolve().parent
-        print(parent_dir)
-    if args.cwd:
-        print(Path.cwd())
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
